c7df0581b4e9249b1ca631d5e34727101ac327aa
YevheniiYevtushenko <yevheniiyevtushenkov@gmail.com>
2022-10-24 01:21:32 +0300

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,12 @@ public class Main {
         //Character.isUpperCase('t') перевіряє чи знаходиться символ у верхньому регістрі
         System.out.println(Character.isUpperCase('t'));
         System.out.println(Character.isUpperCase('T'));
-
-
+        System.out.println(" " + "\uD83D\uDE80" + " " + "\uD83D\uDE37" + " " + "\uD83D\uDE2B");
+        char s1 = (char)((Math.random() * (65535 - 32)) + 0);
+        char s2 = (char)((Math.random() * (65535 - 32)) + 0);
+        int res = Character.compare(s1, s2);
+        if(res > 0) System.out.println(s1 + " > " + s2);
+        else if(res < 0) System.out.println(s1 + " < " + s2);
+        else System.out.println(s1 + " = " + s2);
     }
-
 }

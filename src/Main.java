26e30821e9fef0acea8f7fad577a1f958c1dc3b9
YevheniiYevtushenko <yevheniiyevtushenkov@gmail.com>
2022-10-24 00:35:54 +0300

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Character.isUpperCase('t') перевіряє чи знаходиться символ у верхньому регістрі
+
+
     }
 
 }
